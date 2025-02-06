0efbc31f26461b42198823afb70bd8c8c0de96e7
Cisa <galuhsaputra050508@gmail.com>
2025-02-06 17:59:57 +0700

    Fix endpoint

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const axios = require('axios');
 
 const app = express();
 
-app.use(cors({
-    origin: "*",
-    methods: "GET",
-    allowedHeaders: "Content-Type"
-}));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('API is working!');
+});
 
 app.get('/genshin', async (req, res) => {
     try {
         const response = await axios.get('https://gsi.fly.dev/');
         res.json(response.data);
     } catch (error) {
+        console.error('Error fetching Genshin data:', error);
         res.status(500).json({ error: 'Error fetching data' });
     }
 });
