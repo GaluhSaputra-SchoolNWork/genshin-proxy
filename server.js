a747c6b5d0f7db2414bc2c6898745221643378c7
Cisa <galuhsaputra050508@gmail.com>
2025-02-06 18:04:00 +0700

    ...

--- server.js
+++ server.js
@@ -3,19 +3,13 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-
 app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('API is working!');
-});
 
 app.get('/genshin', async (req, res) => {
     try {
         const response = await axios.get('https://gsi.fly.dev/');
         res.json(response.data);
     } catch (error) {
-        console.error('Error fetching Genshin data:', error);
         res.status(500).json({ error: 'Error fetching data' });
     }
 });
